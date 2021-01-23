49c963e00636639ca8ffdc58d879c7fb5ca0f4cc
GrahamHutchinso6275 <grahamhutch123@gmail.com>
2021-01-23 19:11:40 +0000

    Initial Setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: "hello, world!"
+  include SessionsHelper
+
+  private
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
   end
 end
