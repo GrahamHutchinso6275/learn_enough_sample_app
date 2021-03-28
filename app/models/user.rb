4602e454491dc162879c38a6dc0c16dda39d590e
GrahamHutchinso6275 <grahamhutch123@gmail.com>
2021-03-28 13:42:44 +0100

    Feed setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -74,17 +74,19 @@ class User < ApplicationRecord
     UserMailer.password_reset(self).deliver_now
   end
 
+  # Returns true if a password reset has expired.
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
 
+  # Returns a user's status feed.
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
   end
 
   # Follows a user.
   def follow(other_user)
-    following << other_user unless self == other_user
+    following << other_user
   end
 
   # Unfollows a user.
